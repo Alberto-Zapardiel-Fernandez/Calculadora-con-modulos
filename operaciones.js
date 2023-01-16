509d07b66d13b7262e99ded58c51515669369c4c
Alberto-Zapardiel-Fernandez <alberto.zapardiel.fernandez@gmail.com>
2023-01-16 17:14:57 +0100

    Añadida regex para identificar números

--- operaciones.js
+++ operaciones.js
@@ -1,4 +1,6 @@
+const regex = /^[0-9]$/;
 export const sumar = (a, b) => a + b;
 export const restar = (a, b) => a - b;
 export const multiplicar = (a, b) => a * b;
 export const dividir = (a, b) => a / b;
+export const esNumero = (numero) => (numero.match(regex) ? true : false);
